c8e99dbc81799ef1f6f391a4da928d95a4b8ed7e
Andreas Holstenson <a@holstenson.se>
2019-04-14 08:16:10 +0200

    feat(decimal): MathContext support for add

--- src/decimal/add.ts
+++ src/decimal/add.ts
@@ -3,11 +3,19 @@ import { validateCompatible } from './validateCompatible';
 import { SPI } from './ops/symbols';
 
 import { addOp } from './ops/add';
+import { MathContext } from '../context';
 
 /**
  * Add two decimal numbers together.
+ *
+ * @param a
+ *   the first number
+ * @param b
+ *   the second number
+ * @param context
+ *   optional context with information about scale/precision
  */
-export function add<C, D extends BaseDecimal<C>>(a: D, b: D): D {
+export function add<D extends BaseDecimal<any>>(a: D, b: D, context?: MathContext): D {
 	validateCompatible(a, b);
-	return addOp(a[SPI], a, b);
+	return addOp(a[SPI], a, b, context);
 }
--- src/decimal/ops/add.ts
+++ src/decimal/ops/add.ts
@@ -1,6 +1,10 @@
-import { EXPONENT, COEFFICIENT } from './symbols';
 import { BaseDecimal } from '../decimal-base';
 import { DecimalSPI } from '../decimal-spi';
+
+import { MathContext } from '../../context';
+
+import { EXPONENT, COEFFICIENT } from './symbols';
+import { rescaleCoefficientAndExponent } from './rescaling';
 
 /**
  * Operation that adds two decimals together.
@@ -8,24 +12,33 @@ import { DecimalSPI } from '../decimal-spi';
  * @param a
  * @param b
  */
-export function addOp<C, D extends BaseDecimal<C>>(math: DecimalSPI<C, D>, a: D, b: D): D {
+export function addOp<C, D extends BaseDecimal<C>>(
+	spi: DecimalSPI<C, D>,
+	a: D,
+	b: D,
+	context?: MathContext
+): D {
 	const aExp = a[EXPONENT];
 	const bExp = b[EXPONENT];
-
 	const diff = aExp - bExp;
+
+	let coefficient;
+	let exponent;
 	if(diff === 0) {
 		// The same exponent results a simple add between the coefficients
-		const r = math.add(a[COEFFICIENT], b[COEFFICIENT]);
-		return math.newInstance(r, aExp);
+		coefficient = spi.add(a[COEFFICIENT], b[COEFFICIENT]);
+		exponent = aExp;
 	} else if(diff > 0) {
 		// Need to align the coefficients and a's exponent is bigger
-		const alignedA = math.multiply(a[COEFFICIENT], math.exponentiate(math.TEN, math.wrap(diff)));
-		const r = math.add(alignedA, b[COEFFICIENT]);
-		return math.newInstance(r, bExp);
+		const alignedA = spi.multiply(a[COEFFICIENT], spi.exponentiate(spi.TEN, spi.wrap(diff)));
+		coefficient = spi.add(alignedA, b[COEFFICIENT]);
+		exponent = bExp;
 	} else {
 		// Need to align the coefficients and b's exponent is bigger
-		const alignedB = math.multiply(b[COEFFICIENT], math.exponentiate(math.TEN, math.wrap(-diff)));
-		const r = math.add(a[COEFFICIENT], alignedB);
-		return math.newInstance(r, aExp);
+		const alignedB = spi.multiply(b[COEFFICIENT], spi.exponentiate(spi.TEN, spi.wrap(-diff)));
+		coefficient = spi.add(a[COEFFICIENT], alignedB);
+		exponent = aExp;
 	}
+
+	return rescaleCoefficientAndExponent(spi, coefficient, exponent, context);
 }
