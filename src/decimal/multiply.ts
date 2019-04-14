ee7252fb53a9a35f8398135d01fcc97cd33f1bd8
Andreas Holstenson <a@holstenson.se>
2019-04-14 08:16:48 +0200

    feat(decimal): MathContext support for multiply

--- src/decimal/multiply.ts
+++ src/decimal/multiply.ts
@@ -3,8 +3,9 @@ import { validateCompatible } from './validateCompatible';
 import { SPI } from './ops/symbols';
 
 import { multiplyOp } from './ops/multiply';
+import { MathContext } from '../context';
 
-export function multiply<C, D extends BaseDecimal<C>>(a: D, b: D) {
+export function multiply<D extends BaseDecimal<any>>(a: D, b: D, context?: MathContext) {
 	validateCompatible(a, b);
-	return multiplyOp(a[SPI], a, b);
+	return multiplyOp(a[SPI], a, b, context);
 }
--- src/decimal/ops/multiply.ts
+++ src/decimal/ops/multiply.ts
@@ -1,6 +1,10 @@
-import { EXPONENT, COEFFICIENT } from './symbols';
 import { BaseDecimal } from '../decimal-base';
 import { DecimalSPI } from '../decimal-spi';
+
+import { EXPONENT, COEFFICIENT } from './symbols';
+
+import { MathContext } from '../../context';
+import { rescaleCoefficientAndExponent } from './rescaling';
 
 /**
  * Operation that multiplies two values with each other.
@@ -8,8 +12,14 @@ import { DecimalSPI } from '../decimal-spi';
  * @param {BaseDecimal} a
  * @param {BaseDecimal} b
  */
-export function multiplyOp<C, D extends BaseDecimal<C>>(math: DecimalSPI<C, D>, a: D, b: D): D {
-	const coefficient = math.multiply(a[COEFFICIENT], b[COEFFICIENT]);
+export function multiplyOp<C, D extends BaseDecimal<C>>(
+	spi: DecimalSPI<C, D>,
+	a: D,
+	b: D,
+	context?: MathContext
+): D {
+	const coefficient = spi.multiply(a[COEFFICIENT], b[COEFFICIENT]);
 	const exponent = a[EXPONENT] + b[EXPONENT];
-	return math.newInstance(coefficient, exponent);
+
+	return rescaleCoefficientAndExponent(spi, coefficient, exponent, context);
 }
