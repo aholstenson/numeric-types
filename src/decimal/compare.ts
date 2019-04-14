3abde0b6672e72d55ee680e9ce4c34378b580370
Andreas Holstenson <a@holstenson.se>
2019-04-14 19:41:01 +0200

    feat(decimal): Documentation for comparisons

--- src/decimal/compare.ts
+++ src/decimal/compare.ts
@@ -3,27 +3,64 @@ import { validateCompatible } from './validateCompatible';
 import { SPI } from './ops/symbols';
 import { compareOp } from './ops/compare';
 
-export function compare<D extends BaseDecimal<any>>(a: D, b: D): number {
+/**
+ * Compare two decimal numbers. Returns `0` if the numbers are equal, `-1`
+ * if `a` is less than `b` and `1` if `a` is greater than `b`.
+ *
+ * @param a
+ * @param b
+ */
+export function compare<D extends BaseDecimal<any>>(a: D, b: D): -1 | 0 | 1 {
 	validateCompatible(a, b);
 	return compareOp(a[SPI], a, b);
 }
 
+/**
+ * Get if the given decimal numbers are equal.
+ *
+ * @param a
+ * @param b
+ */
 export function isEqual<D extends BaseDecimal<any>>(a: D, b: D): boolean {
 	return compare(a, b) === 0;
 }
 
+/**
+ * Get if the first decimal number is less than the second one.
+ *
+ * @param a
+ * @param b
+ */
 export function isLessThan<D extends BaseDecimal<any>>(a: D, b: D): boolean {
 	return compare(a, b) < 0;
 }
 
+/**
+ * Get if the first decimal number is less than or equal to the second one.
+ *
+ * @param a
+ * @param b
+ */
 export function isLessThanOrEqual<D extends BaseDecimal<any>>(a: D, b: D): boolean {
 	return compare(a, b) <= 0;
 }
 
+/**
+ * Get if the first decimal number is greater than the second one.
+ *
+ * @param a
+ * @param b
+ */
 export function isGreaterThan<D extends BaseDecimal<any>>(a: D, b: D): boolean {
 	return compare(a, b) > 0;
 }
 
+/**
+ * Get if the first decimal number is greater than or equal to the second one.
+ *
+ * @param a
+ * @param b
+ */
 export function isGreaterThanOrEqual<D extends BaseDecimal<any>>(a: D, b: D): boolean {
 	return compare(a, b) >= 0;
 }
